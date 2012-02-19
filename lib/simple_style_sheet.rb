c2f4999f574ee52529d6059c145991ffcfd66a18
Jacek Mikrut <jacekmikrut.software@gmail.com>
2012-02-19 16:08:28 +0100

    Implemented SimpleStyleSheet::Selector class.

--- lib/simple_style_sheet.rb
+++ lib/simple_style_sheet.rb
@@ -2,6 +2,7 @@ require "simple_style_sheet/version"
 
 require "simple_style_sheet/selector_specificity"
 require "simple_style_sheet/selector_segment"
+require "simple_style_sheet/selector"
 
 module SimpleStyleSheet
 end
